ef158dafe9e660e5f9482f4d0ad4945b03954e3f
IGEG <igor127@bk.ru>
2024-02-01 20:59:49 +0300

    Add project files.

---
+++ Domain/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿
+namespace IGEG.ToolShop.Domain.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using IGEG.ToolShop.Domain.Common;
+
+namespace IGEG.ToolShop.Domain.Entities
+{
+    public class Category : BaseEntity
+    {
+        public int IdCategory { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public string? Img { get; set; }
+        public string Url { get; set; }
+    }
+}
