b333783a3986ab957470e62cfffc712d8c2ff1fd
IGEG <igor127@bk.ru>
2024-02-01 21:27:40 +0300

    added Repo

---
+++ IGEG.ToolShop.Application/Contracts/Percistance/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿using IGEG.ToolShop.Domain.Entities;
+
+namespace IGEG.ToolShop.Application.Contracts.Percistance
+{
+    public interface IProductRepository : IRepository<Product>
+    {
+
+    }
+}
---
+++ IGEG.ToolShop.Application/Contracts/Percistance/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IGEG.ToolShop.Application.Contracts.Percistance
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int Id);
+        Task<T> CreateAsync(int Id);
+        Task<T> UpdateAsync(int Id);
+        Task<T> DeleteAsync(int Id);
+    }
+}
