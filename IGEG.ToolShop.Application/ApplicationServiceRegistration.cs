3f960008eda2387328cdbacc6a99a44fe53edb78
IGEG <igor127@bk.ru>
2024-02-03 16:16:00 +0300

    add mapper, mediatr

---
+++ IGEG.ToolShop.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace IGEG.ToolShop.Application
+{
+    public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ IGEG.ToolShop.Application/MappingProfile/ToolShopProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using IGEG.ToolShop.Domain.Entities;
+
+namespace IGEG.ToolShop.Application.MappingProfile
+{
+    public class ToolShopProfile : Profile
+    {
+        public ToolShopProfile()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Category, CategoryDto>().ReverseMap();
+            CreateMap<ProductModel, ProductModelDto>().ReverseMap();
+            CreateMap<ProductOption, ProductOptionDto>().ReverseMap();
+        }
+    }
+}
