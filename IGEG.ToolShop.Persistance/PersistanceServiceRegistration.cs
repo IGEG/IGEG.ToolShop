c987cc318785a8b2ffda46b88013c4d9e1849092
IGEG <igor127@bk.ru>
2024-02-08 21:03:52 +0300

    add Infrastructure, Persistance

---
+++ IGEG.ToolShop.Persistance/PersistanceServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace IGEG.ToolShop.Persistance
+{
+    public static class PersistanceServiceRegistration
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
