85617985448633058dde305611a224cd30332449
IGEG <igor127@bk.ru>
2024-02-12 21:05:25 +0300

    added infastructure

---
+++ IGEG.ToolShop.Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace IGEG.ToolShop.Infrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
