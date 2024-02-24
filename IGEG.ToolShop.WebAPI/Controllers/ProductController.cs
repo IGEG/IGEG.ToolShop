e2d8002c612549b278c8dc1ceb472816533273e6
IGEG <igor127@bk.ru>
2024-02-24 18:56:55 +0300

    add controller

---
+++ IGEG.ToolShop.WebAPI/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿using IGEG.ToolShop.Application.Dtos;
+using IGEG.ToolShop.Application.Features.Product.Queries.GetAllProducts;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace IGEG.ToolShop.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public ProductController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+        // GET: api/<ProductController>
+        [HttpGet]
+        public async Task<List<ProductDto>> Get()
+        {
+            return await _mediator.Send(new GetAllProductsQuery());
+        }
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
