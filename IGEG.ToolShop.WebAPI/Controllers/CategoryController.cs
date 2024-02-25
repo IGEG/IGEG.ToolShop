a8285503f509b701caffef2cee7f0f1a52361bd6
IGEG <igor127@bk.ru>
2024-02-25 18:36:03 +0300

    added CategoryController

--- IGEG.ToolShop.WebAPI/Controllers/CategoryController.cs
+++ IGEG.ToolShop.WebAPI/Controllers/CategoryController.cs
@@ -1,10 +1,9 @@
 ﻿using IGEG.ToolShop.Application.Dtos;
-using IGEG.ToolShop.Application.Features.Product.Commands.CreateProduct;
-using IGEG.ToolShop.Application.Features.Product.Commands.DeleteProduct;
-using IGEG.ToolShop.Application.Features.Product.Commands.UpdateProduct;
-using IGEG.ToolShop.Application.Features.Product.Queries.GetAllProducts;
-using IGEG.ToolShop.Application.Features.Product.Queries.GetProductById;
-using IGEG.ToolShop.Application.Features.Product.Queries.GetProductsByUrl;
+using IGEG.ToolShop.Application.Features.Category.Commands.CreateCategory;
+using IGEG.ToolShop.Application.Features.Category.Commands.DeleteCategory;
+using IGEG.ToolShop.Application.Features.Category.Commands.UpdateCategory;
+using IGEG.ToolShop.Application.Features.Category.Queries.GetAllCategories;
+using IGEG.ToolShop.Application.Features.Category.Queries.GetCategoryByUrl;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
@@ -14,66 +13,66 @@ namespace IGEG.ToolShop.WebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ProductController : ControllerBase
+    public class CategoryController : ControllerBase
     {
         private readonly IMediator _mediator;
 
-        public ProductController(IMediator mediator)
+        public CategoryController(IMediator mediator)
         {
             _mediator = mediator;
         }
-        // GET: api/<ProductController>
+        // GET: api/<CategoryController>
         [HttpGet]
-        public async Task<List<ProductDto>> Get()
+        public async Task<List<CategoryDto>> Get()
         {
-            return await _mediator.Send(new GetAllProductsQuery());
+            return await _mediator.Send(new GetAllCategoriesQuery());
         }
 
-        // GET api/<ProductController>/5
+        // GET api/<CategoryController>/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<ProductDto>> Get(int id)
+        public string Get(int id)
         {
-            var product = await _mediator.Send(new GetProductByIdQuery(id));
-            return Ok(product);
+            return "value";
         }
 
         // GET api/<ProductController>/category/url
         [HttpGet("category/{Url}")]
-        public async Task<ActionResult<List<ProductDto>>> GetProductsByUrl(string Url)
+        public async Task<ActionResult<CategoryDto>> GetCategoryByUrl(string Url)
         {
-            return await _mediator.Send(new GetProductsByUrlQuery(Url));
+            return await _mediator.Send(new GetCategoryByUrlQuery(Url));
         }
 
-        // POST api/<ProductController>
+        // POST api/<CategoryController>
+        [HttpPost]
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesDefaultResponseType]
-        public async Task<ActionResult> Post(CreateProductCommand createCommand)
+        public async Task<ActionResult> Post(CreateCategoryCommand createCommand)
         {
             var response = await _mediator.Send(createCommand);
             return CreatedAtAction(nameof(Get), new { id = response });
         }
 
-        // PUT api/<ProductController>/5
+        // PUT api/<CategoryController>/5
         [HttpPut("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult> Put(UpdateProductCommand updateCommand)
+        public async Task<ActionResult> Put(UpdateCategoryCommand command)
         {
-            await _mediator.Send(updateCommand);
+            await _mediator.Send(command);
             return NoContent();
         }
 
-        // DELETE api/<ProductController>/5
+        // DELETE api/<CategoryController>/5
         [HttpDelete("{id}")]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult> Delete(int id)
         {
-            await _mediator.Send(new DeleteProductCommand() { Id = id });
+            await _mediator.Send(new DeleteCategoryCommand() { Id = id });
             return NoContent();
         }
     }
