5c1513ff1bb7416092a562f1eb9bf0fd44b13507
IGEG <igor127@bk.ru>
2024-03-11 20:41:07 +0300

    Changed test project

---
+++ IGEG.ToolShop.Application.UnitTests/Features/Product/Queries/GetAllProductsQueryHandlerTest.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using IGEG.ToolShop.Application.Contracts.Percistance;
+using IGEG.ToolShop.Application.Dtos;
+using IGEG.ToolShop.Application.Features.Product.Queries.GetAllProducts;
+using IGEG.ToolShop.Application.Logging;
+using IGEG.ToolShop.Application.MappingProfile;
+using IGEG.ToolShop.Application.UnitTests.Mocks;
+using Moq;
+using Shouldly;
+
+namespace IGEG.ToolShop.Application.UnitTests.Features.Product.Queries
+{
+    public class GetAllProductsQueryHandlerTest
+    {
+        private readonly Mock<IProductRepository> _mockRepo;
+        private readonly IMapper _mapper;
+        private readonly Mock<IAppLogger<GetAllProductsQueryHandler>> _logger;
+
+        public GetAllProductsQueryHandlerTest()
+        {
+            _mockRepo = MockProductRepository.GetMockProductRepository();
+            var mapperConfig = new MapperConfiguration(x =>
+            {
+                x.AddProfile<ProductProfile>();
+            });
+            _mapper = mapperConfig.CreateMapper();
+            _logger = new Mock<IAppLogger<GetAllProductsQueryHandler>>();
+        }
+
+        [Fact]
+        public async Task GetAllProductTest()
+        {
+            var handler = new GetAllProductsQueryHandler(_mapper, _mockRepo.Object, _logger.Object);
+            var result = await handler.Handle(new GetAllProductsQuery(), CancellationToken.None);
+            result.ShouldBeOfType<List<ProductDto>>();
+            result.Count.ShouldBe(3);
+        }
+    }
+}
---
+++ IGEG.ToolShop.Application.UnitTests/Mocks/MockProductRepository.cs
@@ -0,0 +1,62 @@
+﻿using IGEG.ToolShop.Application.Contracts.Percistance;
+using IGEG.ToolShop.Domain.Models;
+using Moq;
+
+namespace IGEG.ToolShop.Application.UnitTests.Mocks
+{
+    public class MockProductRepository
+    {
+        public static Mock<IProductRepository> GetMockProductRepository()
+        {
+            var products = new List<Product>()
+            {
+                new Product
+                {
+                    Id = 1,
+                    Name = "Name1",
+                    SmallDescription = "SmallDescription1",
+                    BigDescription = "BigDescription1",
+                    Images = "Images1",
+                    URL = "URL1",
+                    Volume = 1,
+                    CategoryId = 1
+                },
+                 new Product
+                 {
+                    Id = 2,
+                    Name = "Name2",
+                    SmallDescription = "SmallDescription2",
+                    BigDescription = "BigDescription2",
+                    Images = "Images2",
+                    URL = "URL2",
+                    Volume = 2,
+                    CategoryId = 2
+                 },
+                 new Product
+                 {
+                    Id = 3,
+                    Name = "Name3",
+                    SmallDescription = "SmallDescription3",
+                    BigDescription = "BigDescription3",
+                    Images = "Images3",
+                    URL = "URL3",
+                    Volume = 3,
+                    CategoryId = 3
+                },
+            };
+
+            var mockRepo = new Mock<IProductRepository>();
+
+            mockRepo.Setup(x => x.GetAllAsync()).ReturnsAsync(products);
+
+            mockRepo.Setup(x => x.CreateAsync(It.IsAny<Product>())).
+                Returns((Product product) => 
+                {
+                    products.Add(product);
+                    return Task.CompletedTask;
+                });
+
+            return mockRepo;
+        }
+    }
+}
